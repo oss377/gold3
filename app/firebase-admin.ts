40b571ba58c066e7c656805cdf5b3a06cf1757fa
aweke <awekeadisie@gmail.com>
2025-09-23 03:39:15 -0700

    mm

--- app/firebase-admin.ts
+++ app/firebase-admin.ts
@@ -1,21 +1,30 @@
 import { cert, initializeApp, getApps, App } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import { getStorage } from 'firebase/storage';
+import { getStorage } from 'firebase-admin/storage';
 
 // Initialize with your service account key
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
-);
+let serviceAccount;
+try {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '');
+} catch (error) {
+  console.error('Error parsing FIREBASE_SERVICE_ACCOUNT_KEY:', error);
+  throw new Error('Invalid Firebase service account key');
+}
 
 // Initialize only once
 let adminApp: App;
 
 if (!getApps().length) {
-  adminApp = initializeApp({
-    credential: cert(serviceAccount),
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  });
+  try {
+    adminApp = initializeApp({
+      credential: cert(serviceAccount),
+      databaseURL: process.env.FIREBASE_DATABASE_URL, // Use non-public env var
+    });
+  } catch (error) {
+    console.error('Error initializing Firebase Admin SDK:', error);
+    throw error;
+  }
 } else {
   adminApp = getApps()[0];
 }
